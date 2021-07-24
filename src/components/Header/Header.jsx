c5007b857cd741bd246f0f1309a4fe29e624e935
Will <28814921+wkey1980@users.noreply.github.com>
2021-07-24 20:07:11 +0100

    Built & Styled the header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/Header.Styles.css'
+
+const Header = () => {
+    return (
+        <div>
+            <span className="header">🎬 Entertainment Hub 🎥</span>
+        </div>
+    )
+}
+
+export default Header
