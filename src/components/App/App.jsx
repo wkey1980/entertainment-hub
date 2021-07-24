59f31ab1096849cff97e37745db452d27bec9237
Will <28814921+wkey1980@users.noreply.github.com>
2021-07-24 20:51:59 +0100

    Added BrowserRouter to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,17 @@
 import Header from '../Header';
 import './styles/App.Styles.css';
 import MainNav from '../MainNav/index';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <div className="app">
         <div className="app">Test</div>
         <MainNav />
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
